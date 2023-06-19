2532fe738e4e1d907c79ac834a748b67c5713963
Crowdbotics <team@crowdbotics.com>
2023-06-19 14:12:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-frog-41592",
+  url: "https://hidden-frog-41592.botics.co",
+  api: "https://hidden-frog-41592.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
